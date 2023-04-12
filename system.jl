0640cf2fc8ed7c8fa4a4f0bd63ff11d303aa970b
Roy Sela Arazi <ryarazi@gmail.com>
2023-04-13 01:53:30 +0300

    Dimension is now a parameter of system

--- system.jl
+++ system.jl
@@ -1,15 +1,15 @@
 include("gas.jl")
 
-mutable struct System
+mutable struct System{D}
     #spatial parameters
-    const L::NTuple{3, Float64} #the size in each dimension
+    const L::NTuple{D, Float64} #the size in each dimension
     const Vol::Float64 #volume of the system
 
     #spatial resolution
-    const Ncell::NTuple{3, Int64} #the number of cells in each dimension
+    const Ncell::NTuple{D, Int64} #the number of cells in each dimension
     const Ncell_tot::Float64 #total number of cells
     const Volcell::Float64 #Volume of cell
-    const dL::NTuple{3, Float64} #the length of a cell in each dimensions
+    const dL::NTuple{D, Float64} #the length of a cell in each dimensions
 
     #particles parameters
     const gas::Gas
@@ -21,9 +21,9 @@ mutable struct System
     r::Matrix{Float64}
     v::Matrix{Float64}
 
-    function System(L::NTuple{3, Float64}, Ncell::NTuple{3, Int64},
+    function System(L::NTuple{D, Float64}, Ncell::NTuple{D, Int64},
                     gas::Gas, ρ::Real, Nsim::Integer, 
-                    v_init::Real)
+                    v_init::Real) where {D}
         n = ρ_to_n(ρ, gas)
         Vol = prod(L)
         Ncell_tot = prod(Ncell)
@@ -32,11 +32,11 @@ mutable struct System
         Nphys = n*Vol
         Neff = Nphys/Nsim
 
-        r = rand(3, Nsim)
-        for i in 1:3 r[i,:] *= L[i] end
+        r = rand(D, Nsim)
+        for i in 1:D r[i,:] *= L[i] end
         
-        v = (rand(Bool, 3, Nsim) .* 2. .- 1.)*v_init
-        new(L, Vol, Ncell, Ncell_tot, Vol/Ncell_tot, dL,
+        v = (rand(Bool, D, Nsim) .* 2. .- 1.)*v_init
+        new{D}(L, Vol, Ncell, Ncell_tot, Vol/Ncell_tot, dL,
             gas, Nsim, Nphys, Neff, r, v)
     end
 end
@@ -44,7 +44,7 @@ end
 
 const kb = 1.380649e-23 #Boltzmann constant
 struct SystemProperties
-    E::Vector{Float64}
+    E::Vector{Float64} #energy of each particle
     Etot::Float64
 
     kT::Float64
